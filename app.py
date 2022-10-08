36789e23160a14ab11a0aaa5203539038b7c85ba
Ali Alnajjar <Ali.nj.95@hotmail.com>
2022-10-08 16:10:29 -0400

    First deliverable
    turning on and off an led light with dynamic image change

--- app.py
+++ app.py
@@ -2,27 +2,41 @@
 # visit http://127.0.0.1:8050/ in your web browser.
 
 from dash import Dash, html, Input, Output, ctx
+import RPi.GPIO as GPIO # Import Raspberry Pi GPIO library
+from time import sleep # Import the sleep function from the time module
+GPIO.setwarnings(False) # Ignore warning for now
+GPIO.setmode(GPIO.BCM) # Use physical pin numbering
+LED=17 
+GPIO.setup(LED, GPIO.OUT, initial=GPIO.LOW)
 
 app = Dash(__name__)
 
 app.layout = html.Div([
-    html.Button('Button 1', id='btn-nclicks-1', n_clicks=0),
-    html.Button('Button 2', id='btn-nclicks-2', n_clicks=0),
-    html.Div(id='container-button-timestamp')
+    html.Button(id='btn-nclicks-1', n_clicks=0),
+    # html.Button('Button 2', id='btn-nclicks-2', n_clicks=0),
+    # html.Div(id='container-button-timestamp')
 ])
 
 @app.callback(
-    Output('container-button-timestamp', 'children'),
+    Output('btn-nclicks-1', 'children'),
     Input('btn-nclicks-1', 'n_clicks'),
-    Input('btn-nclicks-2', 'n_clicks'),
+    # Input('btn-nclicks-2', 'n_clicks'),
 )
-def displayClick(btn1, btn2):
-    msg = "None of the buttons have been clicked yet"
-    if "btn-nclicks-1" == ctx.triggered_id:
-        msg = "Button 1 was most recently clicked"
-    elif "btn-nclicks-2" == ctx.triggered_id:
-        msg = "Button 2 was most recently clicked"
-    return html.Div(msg)
+def displayClick(clicks):
+    # msg = "None of the buttons have been clicked yet"
+    if (clicks % 2 == 0): 
+        GPIO.output(LED, GPIO.HIGH) # Turn on
+        sleep(1) 
+        # msg = "LED is ON"
+        return html.Img(src = app.get_asset_url('light.png'), width=200, height=200),
+        # while True:
+    else:
+        GPIO.output(LED, GPIO.LOW) # Turn off
+        sleep(1) # Sleep for 1 second 
+        # msg = "LED is OFF"
+        return html.Img(src = app.get_asset_url('lightOff.png'), width= 200, height=200),
+        # while True:
+    # html.Div(clicks)
 
 if __name__ == '__main__':
     app.run_server(debug=True)
