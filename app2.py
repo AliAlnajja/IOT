bb4b103f1cd7eb4dc16623fde5c3fe6e14de23b9
Buhbah6 <tonynadeau03@gmail.com>
2022-10-25 21:30:31 -0400

    Phase 2 Final Version

--- app2.py
+++ app2.py
@@ -10,12 +10,14 @@ import smtplib
 import imaplib
 import easyimap as imap
 
+# GLOBAL VARIABLES
 EMAIL_ADDRESS = 'noreply.piservice@gmail.com'
 PASSWORD = 'bvlg sbug wars xozh'
-RECEIVER_ADDRESS = EMAIL_ADDRESS
+RECEIVER_ADDRESS = 'tonynadeau03@gmail.com'
 SENT = False
 FAN_ON = False
 
+# Pi Phase 2 Setup
 GPIO.setwarnings(False) # Ignore warning for now
 GPIO.setmode(GPIO.BCM) # Use physical pin numbering
 LED=17 
@@ -24,22 +26,27 @@ enablePin = 13
 leftPin = 19
 rightPin = 26
 GPIO.setup(LED, GPIO.OUT, initial=GPIO.LOW)
-GPIO.setup(enablePin, GPIO.OUT)
-GPIO.setup(leftPin, GPIO.OUT)
-GPIO.setup(rightPin, GPIO.OUT)
-dht = DHT.DHT(DHTPin) #create a DHT class object    
+GPIO.setup(enablePin, GPIO.OUT, initial=GPIO.LOW)
+GPIO.setup(leftPin, GPIO.OUT, initial=GPIO.LOW)
+GPIO.setup(rightPin, GPIO.OUT, initial=GPIO.HIGH)
+dht = DHT.DHT(DHTPin)    
 
-
+# initial read of DHT data
 dht.readDHT11()
 
+# Main method to facilitate code structuring
 def main():
     app = Dash(__name__)
     app.layout = html.Div([
 
+        ## HTML CONTAINERS ##
+
+        # Light Button
         html.Div([
             html.Button(html.Img(src = app.get_asset_url('light.png')), id='btn-nclicks-1', n_clicks=0),
         ]),
 
+        # Gauges for Temperature and Humidity
         html.Div([
             daq.Gauge(
                 id='temp-gauge',
@@ -59,6 +66,7 @@ def main():
                 max=100,
                 min=0,
             ),
+            # Interval to update gauges live
             dcc.Interval(
                     id='temp-humidity-intervals',
                     interval=5*1000, # in milliseconds
@@ -66,6 +74,7 @@ def main():
             ),
         ]),
 
+        # Fan Button
         html.Div([
             html.Button(id='btn-nclicks-2', n_clicks=0),
             dcc.Interval(
@@ -76,6 +85,9 @@ def main():
         ]),
     ])
 
+    ## CALLBACKS ##
+
+    # Callback for turnung the light on and off (Phase 1)
     @app.callback(Output('btn-nclicks-1', 'children'),
                 Input('btn-nclicks-1', 'n_clicks'),
     )
@@ -89,6 +101,7 @@ def main():
             sleep(1)
             return html.Img(src=app.get_asset_url('lightOff.png'), width=200, height=200),
 
+    # Callback for updating the temperature gauge
     @app.callback(Output('temp-gauge', 'value'),
                 Input('temp-humidity-intervals', 'n_intervals'),
     )
@@ -96,13 +109,17 @@ def main():
         global SENT
         dht.readDHT11()
         value = dht.temperature
-        if value > 24 and not SENT:
-            send_email("Temperature is High", "Would You like to turn on the fan?\nPlease reply with \'Yes\' ot \'No\'.")
+        if value > 24 and not SENT: # If temp exceeds 24 degrees Celsius, send email
+            send_email("Temperature is High", "Would You like to turn on the fan?\nPlease reply with \'Yes\' or \'No\'.")
             SENT = True
-        elif value < 22 and FAN_ON:
+        elif receive_email(): # If email is received, with a response of yes, turn on fan
+            displayMotorClick(2)
+        elif value < 22 and FAN_ON: # If temp is below 22 degrees Celsius, turn off fan
+            SENT = False
             displayMotorClick(1)
         return value
-
+    
+    # Callback for updating the humidity gauge
     @app.callback(Output('humidity-gauge', 'value'),
                   Input('temp-humidity-intervals', 'n_intervals'),
     )
@@ -111,6 +128,7 @@ def main():
         value = dht.humidity
         return value
 
+    # Callback for turning the fan on and off
     @app.callback(Output('btn-nclicks-2', 'children'),
                 Input('btn-nclicks-2', 'n_clicks'),
     )
@@ -127,62 +145,15 @@ def main():
             sleep(1)
             return html.Img(src=app.get_asset_url('motor_off.jpg'), width=200, height=200),
 
-    # @app.callback(Output('btn-nclicks-2', 'status'),
-    #     Input('temp-humidity-intervals', 'n_intervals'),
-    # )
-    # def emailToFan(value):
-    #     response = receive_email()
-    #     if response:
-    #         displayMotorClick(2)
-    #         return 'on'
-    #     return 'off'
-    
-    '''
-    @app.callback(
-        Output('btn-nclicks-1', 'children'),
-        Input('btn-nclicks-1', 'n_clicks'),
-        # Output('my-thermometer-1', 'value'),
-        # Input('thermometer-slider-1', 'value'),
-        # Input('btn-nclicks-2', 'n_clicks'),
-    )
-
-    # @app.callback(Output('my-gauge-1', 'value'))
-
-    # @app.callback(
-    #     Output('my-thermometer-1', 'value'),
-    #     Input('thermometer-slider-1', 'value'),
-    #     # Input('btn-nclicks-2', 'n_clicks'),
-    # )
-
-    def displayClick(clicks):
-        # msg = "None of the buttons have been clicked yet"
-        if (clicks % 2 == 0): 
-            GPIO.output(LED, GPIO.HIGH) # Turn on
-            sleep(1) 
-            # msg = "LED is ON"
-            return html.Img(src = app.get_asset_url('light.png'), width=200, height=200),
-            # while True:
-        else:
-            GPIO.output(LED, GPIO.LOW) # Turn off
-            sleep(1) # Sleep for 1 second 
-            # msg = "LED is OFF"
-            return html.Img(src = app.get_asset_url('lightOff.png'), width= 200, height=200),
-            # while True:
-        # html.Div(clicks)
-
-    def update_output(value):
-        return value
-
-
-    # def update_thermometer(value):
-        
-    #         return value,
-        # return value
-    '''
-
+   
+    ## RUN SERVER ##
     if __name__ == '__main__':
         app.run_server(debug=True)
 
+
+## INDEPENDENT PYTHON METHODS ##
+
+# Method to send email
 def send_email(subject, body):
     with smtplib.SMTP('smtp.gmail.com', 587) as smtp:
         smtp.ehlo()
@@ -194,20 +165,7 @@ def send_email(subject, body):
         smtp.sendmail(EMAIL_ADDRESS, RECEIVER_ADDRESS, msg)
 
 
-def receive_email():
-    server = imap.connect('imap.gmail.com', EMAIL_ADDRESS, PASSWORD)
-
-    for mail in server.listids():
-        email = server.mail(mail)
-        response =  (email.title + email.body).lower()
-        if "yes" in response:
-            return True
-    else:
-        server.quit()
-        delete_email()
-        return False
-
-
+# Method to delete email
 def delete_email():
     mail = imaplib.IMAP4_SSL('imap.gmail.com')
     mail.login(EMAIL_ADDRESS, PASSWORD)
@@ -220,10 +178,26 @@ def delete_email():
 
     for num in data[0].split():
         mail.store(num, '+FLAGS', r'(\Deleted)')
-    print("Successfully deleted email")
 
     mail.expunge()
     mail.close()
     mail.logout()
 
+
+# Method to receive email
+def receive_email():
+    server = imap.connect('imap.gmail.com', EMAIL_ADDRESS, PASSWORD)
+
+    for mail in server.listids():
+        email = server.mail(mail)
+        response =  (email.title + " " + email.body).lower()
+        if "yes" in response:
+            delete_email()
+            return True
+    else:
+        server.quit()
+        delete_email()
+        return False
+
+# Call to main to run the application
 main()
