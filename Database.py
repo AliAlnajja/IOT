25f6d6c1ba547cb8ed07192be5c6d7ce09edb36e
Tony Nadeau <thegoldenrailway@gmail.com>
2022-10-28 20:28:22 -0400

    minor changes to database code. Still in development

--- Database.py
+++ Database.py
@@ -5,16 +5,23 @@ import json
 # Useful link for the future
 # https://www.freecodecamp.org/news/how-to-get-started-with-firebase-using-python/
 
+# Database connection
 cred_obj = firebase_admin.credentials.Certificate('\piservice-firebase-adminsdk-6t0hc-e2400044da.json')
 default_app = firebase_admin.initialize_app(cred_obj, {'databaseURL': 'https://piservice-default-rtdb.firebaseio.com/'})
 
+# Sector of Realtime DB
 ref = db.reference("/")
 
-def create(json_data):
+# Method to create data in the database (will be improved)
+def createUser():
 	db.set(json_data)
 
 def read(querystring):
 
 def update(key, column, value):
+	users = ref.get()
+	for k, v, in users.items():
+		if (v["user_id"] == key):
+			ref.child(k).update({column = value})
 
 def delete(key):
